d3920c608259b6d3cd2b30740ee1f27891d845fe
Freark van der Bos <safariman@gmail.com>
2017-03-18 13:30:31 +0100

    Some changes for testing + closing filepointers

--- rcsh/rcsh.py
+++ rcsh/rcsh.py
@@ -21,7 +21,8 @@ log_stdout = False
 log_stderr = False
 max_duration = 30 # seconds # not implemented
 
-syslog.openlog('rcsh', syslog.LOG_PID, syslog.LOG_AUTH)
+def setup_logging():
+    syslog.openlog('rcsh', syslog.LOG_PID, syslog.LOG_AUTH)
 
 # log to syslog
 # get config from conf.d dir
@@ -49,12 +50,14 @@ def load_whitelists(username, config_dir=DEFAULT_FILTER_DIR):
     exact_allowed = []
     # print(os.path.join(config_dir, '%s.exact*' % username))
     for fn in glob(os.path.join(config_dir, '%s.exact*' % username)):
-        # print(fn)
-        exact_allowed += [c.strip() for c in open(fn, 'r').readlines()]
+        print(fn)
+        with open(fn, 'r') as f:
+            exact_allowed += [c.strip() for c in f.readlines() if not c.strip().startswith('#')]
 
     regex_allowed = []
     for fn in glob(os.path.join(config_dir, '%s.regex*' % username)):
-        regex_allowed += [c.strip() for c in open(fn, 'r').readlines()]
+        with open(fn, 'r') as f:
+            regex_allowed += [c.strip() for c in f.readlines() if not c.strip().startswith('#')]
 
     return exact_allowed, regex_allowed
 
@@ -145,6 +148,9 @@ def invoke33(command_requested, timeout=600):
     """
         invoke using python 3.3+ facilities, like waiting with a timeout.
     """
+
+
+
     # execute command, while logging interaction
     p = subprocess.Popen(command_requested, shell=True, stdin=subprocess.PIPE, stdout=None, stderr=None)
     p.stdin.close() # since we do not allow invoked processes to listen to stdin, we close stdin to the subprocess
@@ -167,66 +173,70 @@ def invoke33(command_requested, timeout=600):
 
     return return_code, stdin_lines
 
-try:
-    username = getpass.getuser()
-    if 'SSH_CLIENT' in os.environ:
-        source_ip = os.environ['SSH_CLIENT'].split()[0]
-    else:
-        source_ip = 'unknown source ip'
-    if len(sys.argv) <= 1:
-        # not called with an argument
-        print('Interactive login not permitted.')
-        # log something about the invocation
-        syslog.syslog('Interactive login not permitted for "%s" from %s' % (username, source_ip))
-        sys.exit(1)
-    elif len(sys.argv) == 3 and sys.argv[1] == '-c': 
-        # normal remote invocation
-        command_requested = sys.argv[2]
-        # print('You attempted to invoke "%s"' % command_requested)
 
-        exact_allowed, regex_allowed, = load_whitelists(username)
 
-        allowed = False
-        if command_requested in exact_allowed:
-            allowed = True
+if __name__ == "__main__":
+    setup_logging()
+    try:
+        username = getpass.getuser()
+        if 'SSH_CLIENT' in os.environ:
+            source_ip = os.environ['SSH_CLIENT'].split()[0]
         else:
-            # check against every regexp
-            for regexp in regex_allowed:
-                if re.match(regexp, command_requested):
-                    allowed = True
-                    break
-        if not allowed:
-            # print('This is not allowed, invoke something like:')
-            # pprint(exact_allowed)
-            # pprint(regex_allowed)
-            syslog.syslog('Invocation not allowed for "%s" from %s: %s' % (username, source_ip, command_requested))
+            source_ip = 'unknown source ip'
+        if len(sys.argv) <= 1:
+            # not called with an argument
+            print('Interactive login not permitted.')
+            # log something about the invocation
+            syslog.syslog('Interactive login not permitted for "%s" from %s' % (username, source_ip))
             sys.exit(1)
+        elif len(sys.argv) == 3 and sys.argv[1] == '-c': 
+            # normal remote invocation
+            command_requested = sys.argv[2]
+            # print('You attempted to invoke "%s"' % command_requested)
 
-        # print('This would have been allowed.')
-        syslog.syslog('Invocation allowed for "%s" from %s: %s' % (username, source_ip, command_requested))
+            exact_allowed, regex_allowed, = load_whitelists(username)
 
-        return_code, stdin_lines = invoke(command_requested)
+            allowed = False
+            if command_requested in exact_allowed:
+                allowed = True
+            else:
+                # check against every regexp
+                for regexp in regex_allowed:
+                    if re.match(regexp, command_requested):
+                        allowed = True
+                        break
+            if not allowed:
+                # print('This is not allowed, invoke something like:')
+                # pprint(exact_allowed)
+                # pprint(regex_allowed)
+                syslog.syslog('Invocation not allowed for "%s" from %s: %s' % (username, source_ip, command_requested))
+                sys.exit(1)
 
-        if stdin_lines:
-            syslog.syslog('Invocation for "%s" from %s attempted to pass data over stdin: %s' % (username, source_ip, repr(stdin_lines)))
+            # print('This would have been allowed.')
+            syslog.syslog('Invocation allowed for "%s" from %s: %s' % (username, source_ip, command_requested))
 
-        syslog.syslog('Invocation allowed for "%s" from %s finished with exit code %d' % (username, source_ip, return_code))
-        sys.exit(return_code)
-        # capture exit code and exit with that same code
-    else:
-        # print('Unexpected invocation.')
-        # log something about the invocation
-        syslog.syslog('Unexpected invocation by "%s" from %s, argv:\n%s' % (username, source_ip, repr(sys.argv)))
+            return_code, stdin_lines = invoke(command_requested)
+
+            if stdin_lines:
+                syslog.syslog('Invocation for "%s" from %s attempted to pass data over stdin: %s' % (username, source_ip, repr(stdin_lines)))
+
+            syslog.syslog('Invocation allowed for "%s" from %s finished with exit code %d' % (username, source_ip, return_code))
+            sys.exit(return_code)
+            # capture exit code and exit with that same code
+        else:
+            # print('Unexpected invocation.')
+            # log something about the invocation
+            syslog.syslog('Unexpected invocation by "%s" from %s, argv:\n%s' % (username, source_ip, repr(sys.argv)))
+            sys.exit(1)
+    except:
+        # log information about invocation and exception
+        # but don't leak information
+        etype, evalue, etb = sys.exc_info()
+        if etype == SystemExit:
+            # normal, just raise
+            raise
+        # print('Unexpected error occured during invocation.')
+        syslog.syslog('Unexpected error occured during invocation by "%s" from %s:\n%s' % (username, source_ip, traceback.format_exc()))
+        # print(etype, evalue, etb)
         sys.exit(1)
-except:
-    # log information about invocation and exception
-    # but don't leak information
-    etype, evalue, etb = sys.exc_info()
-    if etype == SystemExit:
-        # normal, just raise
-        raise
-    # print('Unexpected error occured during invocation.')
-    syslog.syslog('Unexpected error occured during invocation by "%s" from %s:\n%s' % (username, source_ip, traceback.format_exc()))
-    # print(etype, evalue, etb)
-    sys.exit(1)
 
