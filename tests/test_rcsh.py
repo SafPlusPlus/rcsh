94a7d96caabc37d85e93c7eff9a90054068d60e3
Freark van der Bos <safariman@gmail.com>
2017-03-18 13:29:39 +0100

    Some silly little test

--- tests/test_rcsh.py
+++ tests/test_rcsh.py
@@ -9,12 +9,14 @@ Tests for `rcsh` module.
 """
 
 
-import sys
+import os, sys
 import unittest
 
 from rcsh import rcsh
 
-
+HERE = os.path.dirname(__file__)
+DATA_DIR = os.path.join(HERE, 'data')
+TMP_DIR = os.path.join(HERE, 'tmp')
 
 class TestRcsh(unittest.TestCase):
 
@@ -24,5 +26,16 @@ class TestRcsh(unittest.TestCase):
     def tearDown(self):
         pass
 
-    def test_000_something(self):
-        pass
+    def test_000_dummy(self):
+        print(HERE, DATA_DIR, TMP_DIR)
+        return 1
+
+    def test_000_load_whitelists(self):
+        exact_allowed, regex_allowed = rcsh.load_whitelists('testdummy', os.path.join(HERE, 'test-config', 'rcsh.d'))
+        print(exact_allowed, regex_allowed)
+        self.assertEqual(exact_allowed, ['uptime'])
+        self.assertEqual(regex_allowed, ['^ls(?: -[lahtr]+)?$'])
+
+
+if __name__ == "__main__":
+    unittest.main()
